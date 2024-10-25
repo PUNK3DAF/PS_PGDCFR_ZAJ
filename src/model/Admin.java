e9f9cf4fe738349396f0044bce9737dc3af357eb
Vld Mrk <vm20190346@student.fon.bg.ac.rs>
2024-10-25 18:10:54 +0200

    Zajednicki projekat

---
+++ src/model/Admin.java
@@ -0,0 +1,98 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author vldmrk
+ */
+public class Admin implements Serializable {
+
+    private String mail;
+    private String sifra;
+    private String ime;
+    private String prezime;
+
+    public Admin() {
+    }
+
+    public Admin(String mail, String sifra, String ime, String prezime) {
+        this.mail = mail;
+        this.sifra = sifra;
+        this.ime = ime;
+        this.prezime = prezime;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getSifra() {
+        return sifra;
+    }
+
+    public void setSifra(String sifra) {
+        this.sifra = sifra;
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    @Override
+    public String toString() {
+        return "Admin{" + "mail=" + mail + ", sifra=" + sifra + ", ime=" + ime + ", prezime=" + prezime + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Admin other = (Admin) obj;
+        if (!Objects.equals(this.mail, other.mail)) {
+            return false;
+        }
+        if (!Objects.equals(this.sifra, other.sifra)) {
+            return false;
+        }
+        if (!Objects.equals(this.ime, other.ime)) {
+            return false;
+        }
+        return Objects.equals(this.prezime, other.prezime);
+    }
+
+}
